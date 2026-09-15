1873fcf02ddf86ade0b3ccc5f0a2c1d4922ac007
ricardogarciaabadia-boop <ricardogarciaabadia@gmail.com>
2026-09-15 19:22:25 +0200

    Añadir tipos documentales para clasificador OCR

---
+++ app/src/main/java/com/rgapro1/ocaso/DocumentType.java
@@ -0,0 +1,10 @@
+package com.rgapro1.ocaso;
+
+/** Tipos documentales reconocidos por el OCR central. */
+public enum DocumentType {
+    DNI_FRONT,
+    DNI_BACK,
+    POLICY,
+    RECEIPT,
+    UNKNOWN
+}
